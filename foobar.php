fcdefcb281033cbd20655d4b18700f5f3c687348
junaidbinjaman <me@junaidbinjaman.com>
2024-10-07 20:52:02 +0600

    Initial commit with existing local directory

---
+++ foobar.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name
+ *
+ * @package           Foobar
+ * @author            Junaid Bin jaman
+ * @copyright         2024 All Next Ver
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Foobar
+ * Plugin URI:        https://foobar.com/plugin-name
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Junaid Bin Jaman
+ * Author URI:        https://junaidbinjaman.com
+ * Text Domain:       foobar
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Update URI:        https://example.com/my-plugin/
+ * Requires Plugins:  my-plugin, yet-another-plugin
+ */
